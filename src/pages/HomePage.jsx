d72aa61f64af3d3d78638c9c9995b9578809db1f
Marta <dulibicmarta00@gmail.com>
2022-10-30 13:09:43 +0100

    preview rounds timetable and results and team leaderboard

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,57 +1,174 @@
 import React, {useEffect, useState} from "react";
+import Table from 'react-bootstrap/Table';
+import {pageStyle} from "../style/globalStyles";
+import {Box, Tabs, Tab, LinearProgress, Alert, Paper, Button} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
-import {Alert, LinearProgress} from "@mui/material";
+const tableStyle = {
+    border: "5px solid #54428E", width: "800px", height: "200px", margin: "auto", textAlign: "center"
+};
+
+const homepageStyle = {
+    flexDirection: "column",
+    overflow: "hidden",
+    alignItems: "center",
+    background: "linear-gradient(30deg, #54428E, #FF6587)",
+    position: "relative",
+};
+
+const roundsGenerator = (round) => {
+    return (
+        <div>
+            <h3 style={{margin: "1rem", textAlign: "left", color: "#FFFFFF"}}>{round.name}</h3>
+            {round.results.length !== 0 ? (
+                <Table striped bordered hover size="sm" style={tableStyle}>
+                    <thead style={{backgroundColor: "#54428E", color: "#FFFFFF" }}>
+                        <tr>
+                            <th>#</th>
+                            <th>Team 1</th>
+                            <th>Team 2</th>
+                            <th>Team 1 goals</th>
+                            <th>Team 2 goals</th>
+                        </tr>
+                    </thead>
+                    <tbody style={{backgroundColor: "#FFFFFF"}}>
+                    {round.results.map((val, key) => {
+                        return (<tr key={key}>
+                            <td>{key + 1}</td>
+                            <td>{val.team1}</td>
+                            <td>{val.team2}</td>
+                            <td>{val.team1Goals}</td>
+                            <td>{val.team2Goals}</td>
+                        </tr>)
+                    })}
+                    </tbody>
+                </Table>) : (<div></div>)
+            }
+            {round.timetables.length !== 0 ? (
+            <Table striped bordered hover size="sm" style={tableStyle}>
+                <thead style={{backgroundColor: "#54428E", color: "#FFFFFF"}}>
+                <tr>
+                        <th>#</th>
+                        <th>Team 1</th>
+                        <th>Team 2</th>
+                        <th>Time and date</th>
+                </tr>
+                </thead>
+                <tbody style={{backgroundColor: "#FFFFFF"}}>
+                {round.timetables.map((val, key) => {
+                    return (<tr key={key}>
+                        <td>{key + 1}</td>
+                        <td>{val.team1}</td>
+                        <td>{val.team2}</td>
+                        <td>{val.timeAndDate}</td>
+                    </tr>)
+                })}
+                </tbody>
+            </Table>) : (<div></div>)
+            }
+        </div>
+     );
+};
 
 function HomePage() {
     const api = process.env.REACT_APP_API_URL;
-    const [friends, setFriends] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [errorMessage, setErrorMessage] = useState(false);
+    const [teams, setTeams] = useState([]);
+    const [rounds, setRounds] = useState([]);
 
-    /*
+    const navigate = useNavigate();
+
+    const { logout } = useAuth0();
+
     useEffect(() => {
-      fetch(api + "/users/friends", {
-        method: "GET",
-        headers: {
-          Authorization: authHeader(),
-          Origin: origin,
-        },
-      })
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            setErrorMessage(true);
-            throw new Error(response.status);
-          }
+        fetch(api + "/teams", {
+            method: "GET", headers: {
+                Origin: origin
+            },
         })
-        .then((data) => {
-          setFriends(data);
-          setLoading(false);
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error(response.status);
+                }
+            })
+            .then((data) => {
+                setTeams(data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, [api]);
+
+    useEffect(() => {
+        fetch(api + "/leagueRound", {
+            method: "GET", headers: {
+                Origin: origin
+            },
         })
-        .catch((err) => {
-          console.log(err);
-          setLoading(false);
-        });
-    }, []);
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error(response.status);
+                }
+            })
+            .then((data) => {
+                setRounds(data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, [api]);
 
-     */
 
-    return (<div style={{flexGrow: 1, textAlign: "center"}}>
-        <h1 style={{marginBottom: "4rem"}}>Followed users</h1>
-        {loading ? (<LinearProgress style={{marginTop: "1rem"}}/>) : errorMessage ? (
-            <Alert variant="outlined" severity="error">
-                Error occurred while communicating with the server.
-            </Alert>) : (<>
+    return (
+        <Box sx={{ width: "100%" }}>
+        <div style={{...pageStyle, ...homepageStyle, overflow:"scroll", paddingBottom:"20px"}}>
+            <div style={{ display: "flex", backgroundColor:"#54428E"}}>
 
-            <div style={{margin: "auto", marginTop: "2rem", width: "75%"}}/>
+                <h1 style={{margin: "1rem", textAlign: "left", color: "#FFFFFF"}}>MySportOverview</h1>
 
-            <Alert variant="outlined" severity="info">
-                No users are followed.
-            </Alert>
-            )}
-        </>)}
-    </div>);
+                <Button
+                    style={{ margin: "0.3rem", backgroundColor: "#FF6587"}}
+                    onClick={() => logout({ returnTo: window.location.origin })}
+                    variant="contained"
+                >
+                    Log out
+                </Button>
+            </div>
+
+        <h1 style={{margin: "1rem", textAlign: "left", color: "#FFFFFF"}}>Leaderboard</h1>
+
+        <Table striped bordered hover size="sm" style={tableStyle}>
+            <thead style={{backgroundColor: "#54428E", color: "#FFFFFF"}}>
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Points</th>
+                <th>Goals</th>
+            </tr>
+            </thead>
+            <tbody style={{backgroundColor: "#FFFFFF"}}>
+            {teams.sort((opt1, opt2) => opt2.points - opt1.points).map((val, key) => {
+                return (<tr key={key}>
+                    <td>{key + 1}</td>
+                    <td>{val.name}</td>
+                    <td>{val.points}</td>
+                    <td>{val.goals}</td>
+                </tr>)
+            })}
+            </tbody>
+        </Table>
+
+        <h1 style={{margin: "1rem", textAlign: "left", color: "#FFFFFF"}}>Results and timetable</h1>
+
+        {rounds.map((val, key) => {
+            return roundsGenerator(val)
+        })}
+    </div>
+        </Box>);
 }
 
 export default HomePage;
